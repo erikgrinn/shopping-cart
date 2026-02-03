75082392775b0394c25c4e96e6ab566fe03e7639
Erik Grinn <grinn.erik@gmail.com>
2026-02-02 19:37:54 -0500

    forgot to include value for minus button

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -6,31 +6,23 @@ import styles from "../styles/cards.module.css";
 function Cards() {
   const { fakeStoreData, setFakeStoreData, cartData, setCartData } = useOutletContext();
   function handleClick(e) {
-    console.log(e.target.value);
     const id = e.target.value;
     const current = cartData[id] || 0;
-    // getting issues for minus
     if (e.target.textContent === "-") {
       if (current === 0) return;
-      setCartData((prev) => {
-        const current = prev[id] || 0;
-        return {
-          ...prev,
-          number: prev.number - 1,
-          [id]: current - 1,
-        };
-      });
+      setCartData((prev) => ({
+        ...prev,
+        number: prev.number - 1,
+        [id]: current - 1,
+      }));
     } else if (e.target.textContent === "+") {
-      setCartData((prev) => {
-        const current = prev[id] || 0;
-        return {
-          ...prev,
-          number: prev.number + 1,
-          [id]: current + 1,
-        };
-      });
+      setCartData((prev) => ({
+        ...prev,
+        number: prev.number + 1,
+        [id]: current + 1,
+      }));
     }
-    console.log(cartData[id]); // recall this shows before state updates
+    console.log(cartData[id], cartData); // recall this shows before state updates
   }
 
   useEffect(() => {
@@ -49,7 +41,9 @@ function Cards() {
         {fakeStoreData.map((item) => (
           <div className={styles.card} key={item.title}>
             <div className={styles.topCard}>
-              <button onClick={handleClick}>-</button>
+              <button value={item.id} onClick={handleClick}>
+                -
+              </button>
               <img src={item.image} alt={item.title} width={120} height={120} />
               <button value={item.id} onClick={handleClick}>
                 +
